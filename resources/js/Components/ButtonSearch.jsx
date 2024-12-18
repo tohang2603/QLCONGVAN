b85465d89c7a00acfbbcfdb54472a9a078c5eccd
tohang2603 <phanhuynh263@gmail.com>
2024-12-18 21:05:30 +0700

    "giaodien1"

--- resources/js/Components/ButtonSearch.jsx
+++ resources/js/Components/ButtonSearch.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from 'react';
-import ButtonLink from './ButtonLink'; // Assuming ButtonLink is a separate component
-
+import ButtonIcon from './ButtonIcon';
+import { router } from '@inertiajs/react';
 const ButtonSearch = () => {
   const [searchKeyword, setSearchKeyword] = useState('');
 
   return (
-    <div> {/* Thêm container với các class Tailwind */}
+    <div className='grid grid-cols-6'> {/* Thêm container với các class Tailwind */}
       <input
         type="text"
-        className="p-2 border border-gray-300 rounded-lg w-5/6 focus:outline-none focus:ring-2 focus:ring-blue-600 transition duration-150 ease-in-out text-gray-800 placeholder-gray-400"
+        className="p-2 border border-gray-300 rounded-lg col-span-5 focus:outline-none focus:ring-2 focus:ring-blue-600 transition duration-150 ease-in-out text-gray-800 placeholder-gray-400"
         placeholder="Tìm kiếm số công văn, mô tả, hoặc tiêu đề..."
         value={searchKeyword}
         onChange={(e) => setSearchKeyword(e.target.value)}
       />
-      <ButtonLink
-        className=" ml-6 px1 py-0 bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-medium rounded-lg shadow-md transition duration-150 ease-in-out hover:from-indigo-600 hover:to-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600"
-        href={`tim-kiem-cong-van?search=${searchKeyword}`}
+      <ButtonIcon
+        className=" ml-6 px1 py-0 col-span-1 bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-medium rounded-lg shadow-md transition duration-150 ease-in-out hover:from-indigo-600 hover:to-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600"
+        onClick={() => router.get(`tim-kiem-cong-van?search=${searchKeyword}`)}
       >
-        <span className="text-base">Tìm kiếm</span>
-      </ButtonLink>
+        <ion-icon name="search-outline"></ion-icon>
+      </ButtonIcon>
     </div>
   );
 };
